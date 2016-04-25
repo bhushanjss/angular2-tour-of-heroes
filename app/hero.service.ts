26ad3162c9fe3ab2c50e7da377bfd249d0ed67b2
Vidya Singh <bhushanjss@gmail.com>
2016-04-25 15:51:34 -0500

    hero details with navigation.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -12,5 +12,10 @@ export class HeroService{
   		return new Promise<Hero[]>(resolve =>
    			setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
   		);
-	}	
+	}
+	getHero(id: number) {
+		return Promise.resolve(HEROES).then(
+			heroes => heroes.filter(hero => hero.id===id)[0]
+		);
+	}
 }
